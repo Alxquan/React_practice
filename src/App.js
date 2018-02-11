5491efb1f913c81e79f7862159dd4dcdafa7028e
Alex Quan <alxquan@gmail.com>
2018-02-11 00:37:29 -0700

    made it right

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import Hello from './Hello'
 
 class App extends Component {
-  render() {
-  function Hello(props){
-    return <h1>Hello, {props.name}</h1>;
+  constructor(){
+    super()
+    this.state = {name: "world"}
+    //this.handleChange = this.handleChange.bind(this)
+    // same as the handle change function below
   }
-  const element = <Hello name="world"/>;
+
+
+  handleChange = (e) => {
+      this.setState({name: e.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name}/>
+        <input name="name" type="text" onChange={this.handleChange} />
+      </div>
+    )
   }
 }
 
 export default App;
+
+
+function(e) { // do something }
+(e) => { // do something }
